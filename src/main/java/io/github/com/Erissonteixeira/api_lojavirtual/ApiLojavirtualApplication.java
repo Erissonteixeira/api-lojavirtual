5554fe0f4fbda77a6460f9652d18670730f728b7
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-01 17:56:44 -0300

    first commit

---
+++ src/main/java/io/github/com/Erissonteixeira/api_lojavirtual/ApiLojavirtualApplication.java
@@ -0,0 +1,13 @@
+package io.github.com.Erissonteixeira.api_lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLojavirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/com/Erissonteixeira/api_lojavirtual/ApiLojavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.Erissonteixeira.api_lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
