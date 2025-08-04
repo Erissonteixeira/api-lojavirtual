76ec3dc350b615d150f3642257caaba95c156b66
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-04 13:55:12 -0300

    feat: Adiciona model Produto com JPA

---
+++ src/main/java/io/github/com/Erissonteixeira/api_lojavirtual/model/Produto.java
@@ -0,0 +1,62 @@
+package io.github.com.Erissonteixeira.api_lojavirtual.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private int quantidade;
+    private boolean disponivel;
+
+    public Produto(){
+    }
+    public Produto(Long id, String nome, String descricao, int quantidade, boolean disponivel){
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.disponivel = disponivel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
+
