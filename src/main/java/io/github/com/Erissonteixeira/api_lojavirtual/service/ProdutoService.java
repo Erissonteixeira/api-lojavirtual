59db29164fcaba0b4fdaf6d3470b7df00f798436
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-06 20:41:50 -0300

    feat: implementar ProdutoService com métodos de CRUD

--- src/main/java/io/github/com/Erissonteixeira/api_lojavirtual/service/ProdutoService.java
+++ src/main/java/io/github/com/Erissonteixeira/api_lojavirtual/service/ProdutoService.java
@@ -2,27 +2,37 @@ package io.github.com.Erissonteixeira.api_lojavirtual.service;
 
 import io.github.com.Erissonteixeira.api_lojavirtual.model.Produto;
 import io.github.com.Erissonteixeira.api_lojavirtual.repository.ProdutoRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
     private final ProdutoRepository produtoRepository;
-    public ProdutoService(ProdutoRepository produtoRepository){
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
         this.produtoRepository = produtoRepository;
     }
-    public Produto created(Produto produto){
+
+    public Produto created(Produto produto) {
         return produtoRepository.save(produto);
     }
-    public List<Produto> list(){
+
+    public List<Produto> list() {
         return produtoRepository.findAll();
     }
-    public Produto update(Produto produto){
+    public Optional<Produto> getById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public Produto update(Produto produto) {
         return produtoRepository.save(produto);
     }
-    public boolean delete(Long id){
-        if (produtoRepository.existsById(id)){
+
+    public boolean delete(Long id) {
+        if (produtoRepository.existsById(id)) {
             produtoRepository.deleteById(id);
             return true;
         }
