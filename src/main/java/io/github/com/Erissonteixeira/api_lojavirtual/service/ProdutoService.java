0ac25a44fe35e56619781e27d1176e5a83430964
Erissonteixeira <seu-email-erisson.trabalhos@gmail.com>
2025-08-06 15:11:36 -0300

    feat: implementar métodos CRUD na service de Produto

--- src/main/java/io/github/com/Erissonteixeira/api_lojavirtual/service/ProdutoService.java
+++ src/main/java/io/github/com/Erissonteixeira/api_lojavirtual/service/ProdutoService.java
@@ -1,11 +1,31 @@
 package io.github.com.Erissonteixeira.api_lojavirtual.service;
 
+import io.github.com.Erissonteixeira.api_lojavirtual.model.Produto;
 import io.github.com.Erissonteixeira.api_lojavirtual.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class ProdutoService {
     private final ProdutoRepository produtoRepository;
     public ProdutoService(ProdutoRepository produtoRepository){
         this.produtoRepository = produtoRepository;
+    }
+    public Produto created(Produto produto){
+        return produtoRepository.save(produto);
+    }
+    public List<Produto> list(){
+        return produtoRepository.findAll();
+    }
+    public Produto update(Produto produto){
+        return produtoRepository.save(produto);
+    }
+    public boolean delete(Long id){
+        if (produtoRepository.existsById(id)){
+            produtoRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
